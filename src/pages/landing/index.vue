a95f05534a05168b79d250aa7b1ec295c25ed467
Andrei <soprachev@mail.ru>
2021-09-06 15:22:52 +0300

    landing

--- src/pages/landing/index.vue
+++ src/pages/landing/index.vue
@@ -1,3 +1,12 @@
 <template>
-	<h1>Landing</h1>
+	<div class="main">
+		<h1>Landing</h1>
+		<p>История событий: <router-link to="/statistics">/statistics</router-link></p>
+	</div>
 </template>
+
+<style lang="scss" scoped>
+.main {
+	padding: 10px;
+}
+</style>
