517d028aa25b9e142cc6d16efa3933bd90966bd2
Andrei <soprachev@mail.ru>
2024-01-25 00:21:58 +0300

    Add TotalShots component and update Main and SessionInfographics pages

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
+  <div class="main">
     <h1>WotStat модификация</h1>
   </div>
 </template>
+
+<style scoped lang="scss">
+.main {
+  display: flex;
+  place-items: center;
+  justify-content: center;
+  min-width: 320px;
+  min-height: 100vh;
+}
+</style>
--- src/pages/SessionInfographics.vue
+++ src/pages/SessionInfographics.vue
@@ -1,19 +1,91 @@
 <template>
-  <div>
+  <div class="center-container">
     <h1>Сессионная инфографика</h1>
-    <div class="card">
-      <ShotsCircle />
+    <h2>Стрельба</h2>
+    <div class="flex ver shots">
+      <div class="card long">
+        <TotalShots />
+      </div>
+      <div class="flex hor main">
+        <div class="card circle">
+          <ShotsCircle />
+          <p>- попали</p>
+          <p>- не попали</p>
+        </div>
+        <div class="flex ver flex-1 right">
+          <div class="grid mincards">
+            <div class="card">
+              <p>Попали</p>
+            </div>
+            <div class="card">
+              <p>С уроном</p>
+            </div>
+            <div class="card">
+              <p>В первую половину</p>
+            </div>
+            <div class="card">
+              <p>В первую треть</p>
+            </div>
+          </div>
+          <div class="card chart">
+            <p>ГРАФИК</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import ShotsCircle from "@/components/widgets/ShotsCircle.vue";
+import TotalShots from "@/components/widgets/TotalShots.vue";
 
 </script>
 
 <style lang="scss" scoped>
+@import '@/styles/mixins.scss';
+
 .card {
-  padding: 40px;
+  padding: 15px;
+}
+
+.shots {
+  .card.circle {
+    @include small {
+      width: 300px;
+    }
+
+    @include medium {
+      width: 400px;
+    }
+  }
+
+  .main {
+    @include less-small {
+      flex-direction: column;
+    }
+
+    .right {
+      @include less-small {
+        flex-direction: column-reverse;
+
+        .chart {
+          min-height: 250px;
+        }
+      }
+    }
+  }
+
+  .mincards {
+    @include small {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  .chart {
+    @include small {
+      flex: 1;
+    }
+  }
 }
 </style>
