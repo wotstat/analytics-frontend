728129cc19efce35c0595573db301593763561f0
Andrei <soprachev@mail.ru>
2024-08-09 16:54:44 +0300

    lootbox widget

---
+++ src/components/obs/LootboxInfo.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="flex">
+    <div class="card flex-1" :style="{ backgroundColor: `#${props.backgroundColor}` }">
+      <div class="main gap-0" :style="style">
+        <p class="flex-1">
+          <span class="small">Открыто контейнеров: </span>
+          <span class="big bold num right">{{ infosTween[0] }}</span>
+        </p>
+
+        <div class="flex currencies">
+          <div class="flex-1 num">
+            <p class="line">
+              <img src="/big-icons/gold.png">
+              {{ logProcessor(infosTween[1]) }}
+            </p>
+            <p class="line">
+              <img src="/big-icons/credits.png">
+              {{ logProcessor(infosTween[2]) }}
+            </p>
+          </div>
+
+          <div class="flex-1 num">
+            <p class="line">
+              <img src="/big-icons/freeXP.png">
+              {{ logProcessor(infosTween[3]) }}
+            </p>
+            <p class="line">
+              <img src="/big-icons/premium-icon-wot.png">
+              {{ logProcessor(infosTween[4]) }}
+            </p>
+          </div>
+        </div>
+        <!-- <div class="sum flex-1">
+          <p class="header center grey">Сумма ({{ battleCount }})</p>
+          <p class="num green bold center" :style="{ color: `#${props.color}` }">{{ processor(sumDamageTween) }}</p>
+        </div>
+        <div class="last flex-1 ">
+          <p class="header center grey">Посл. бой</p>
+          <p class="num center white">{{ processor(lastDamageTween) }}</p>
+        </div> -->
+        <!-- <p>Местоf: <span class="green bold num" :style="style">{{ processor(placeTween) }}</span></p> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { useFixedSpaceProcessor, useLogProcessor } from '@/composition/usePercentProcessor';
+import { useTweenCounter } from '@/composition/useTweenCounter';
+import { computed, nextTick, onMounted, ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+const props = defineProps<{
+  count: number
+  premium: number
+  gold: number
+  credits: number
+  freeXp: number
+  tanks: { name: string, count: number }[]
+  color?: string;
+  backgroundColor?: string;
+}>();
+
+const accentColor = computed(() => `#${props.color ?? '7ee927'}`)
+const style = computed(() => ({
+  '--accent-color': accentColor.value
+}))
+
+const processor = useFixedSpaceProcessor(0)
+const logProcessor = useLogProcessor()
+
+const infos = computed(() => ([props.count, props.gold, props.credits, props.freeXp, props.premium]))
+const infosTween = useTweenCounter(infos, { duration: 1 })
+
+// const sumDamage = computed(() => props.sumDamage);
+// const lastDamage = computed(() => props.lastDamage);
+
+// const sumDamageTween = useTweenCounter(sumDamage, { duration: 1 });
+// const lastDamageTween = useTweenCounter(lastDamage, { duration: 1 });
+
+
+const route = useRoute();
+
+onMounted(() => {
+  if (route.meta.clearPage)
+    document.body.style.backgroundColor = 'transparent';
+})
+
+</script>
+
+<style lang="scss" scoped>
+.card {
+  font-size: 1.3vw;
+  padding: 1em 4em;
+  border-radius: 3em;
+  background-color: #292929;
+  position: relative;
+
+
+  .main {
+
+    .currencies {
+      font-size: 6em;
+      margin-top: 0.5em;
+
+      img {
+        width: 1.5em;
+        margin-right: 0.5em;
+      }
+
+      .line {
+        display: flex;
+        align-items: center;
+      }
+    }
+
+    p {
+      font-size: 1em;
+    }
+
+    .small {
+      font-size: 4em;
+    }
+
+    .big {
+      font-size: 8em;
+    }
+
+    .header {
+      font-size: 4em;
+      color: #c4c4c4;
+    }
+
+    .center {
+      text-align: center;
+    }
+
+    .bold {
+      font-weight: bold;
+    }
+
+    .num {
+      font-variant-numeric: tabular-nums;
+      text-wrap: nowrap;
+      white-space: nowrap;
+    }
+
+    .num {
+      line-height: 1.2;
+    }
+
+    .white {
+      color: white;
+    }
+
+    .grey {
+      color: #c4c4c4;
+    }
+  }
+
+  $grey: #c4c4c4;
+
+  .green {
+    color: #7ee927;
+  }
+
+  .accent {
+    color: var(--accent-color)
+  }
+}
+</style>
---
+++ src/pages/obs/LootboxInfo.vue
@@ -0,0 +1,130 @@
+<template>
+  <LootboxInfo ref="widget" v-bind="targetProps" :color="color" :backgroundColor="backgroundColor" v-if="!hasError" />
+  <p class="error-text" v-else>Никнейм не найден. Если вы уверены в нём, возможно баг, напишите на почту
+    soprachev@mail.ru</p>
+  <!-- {{ targetProps }} -->
+</template>
+
+
+<script setup lang="ts">
+import LootboxInfo from '@/components/obs/LootboxInfo.vue';
+import { useQueryStatParams } from '@/composition/useQueryStatParams';
+import { dateToDbIndex, query } from '@/db';
+import { computed, onMounted, onUnmounted, ref, shallowRef, watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+let enabled = true
+
+const params = useQueryStatParams()
+const hasError = ref(false)
+
+const route = useRoute()
+
+const color = computed(() => route.query.color?.toString())
+const backgroundColor = computed(() => route.query.backgroundColor?.toString())
+const containerTag = computed(() => route.query.containerTag?.toString())
+
+function whereClause(ignore: ('player' | 'tag' | 'date')[] = []) {
+  const result = []
+
+  if (containerTag.value && containerTag.value != 'any' && !ignore.includes('tag')) {
+    result.push(`containerTag = '${containerTag.value}'`)
+  }
+
+  if (params.value.player && !ignore.includes('player')) {
+    result.push(`playerName = '${params.value.player}'`)
+  }
+
+  const valueParams = params.value
+
+  if (valueParams.period !== 'allTime' && !ignore.includes('date')) {
+    if (valueParams.period.type == 'fromTo') {
+      result.push(`id >= ${dateToDbIndex(valueParams.period.from)}`);
+      result.push(`id <= ${dateToDbIndex(valueParams.period.to)}`);
+    } else if (valueParams.period.type == 'fromToNow') {
+      result.push(`id >= ${dateToDbIndex(valueParams.period.from)}`);
+    }
+  }
+
+  return result.join(' and ') || 'true'
+}
+
+const targetProps = shallowRef<{
+  count: number
+  gold: number
+  credits: number
+  freeXp: number
+  premium: number
+  tanks: { name: string, count: number }[]
+}>({
+  count: 0,
+  gold: 0,
+  premium: 0,
+  credits: 0,
+  freeXp: 0,
+  tanks: []
+})
+
+async function load() {
+  if (!enabled) return
+
+  try {
+    const total = await query<{ prem: number, count: number, credits: number, freeXP: number, gold: number, vehicles: number }>(`
+      select
+          toUInt32(sum(premiumPlus)) as prem,
+          toUInt32(sum(credits)) as credits,
+          toUInt32(sum(freeXP)) as freeXP,
+          toUInt32(sum(gold)) as gold,
+          sum(length(addedVehicles)) as vehicles,
+          toUInt32(count()) as count
+      from Event_OnLootboxOpen
+      where ${whereClause()}
+      `, { allowCache: false })
+
+    const tanks = await query<{ tag: string, count: number }>(`
+      select tag, toUInt32(count()) as count
+      from Event_OnLootboxOpen
+              array join arrayConcat(addedVehicles, compensatedVehicles.tag) as tag
+      where ${whereClause()}
+      group by tag;`
+    )
+
+    targetProps.value = {
+      premium: total.data[0].prem,
+      count: total.data[0].count,
+      gold: total.data[0].gold,
+      credits: total.data[0].credits,
+      freeXp: total.data[0].freeXP,
+      tanks: tanks.data.map(t => ({ name: t.tag, count: t.count }))
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+
+  setTimeout(() => load(), 5000);
+}
+
+onMounted(() => {
+  load()
+})
+
+onUnmounted(() => {
+  enabled = false
+})
+
+
+
+</script>
+
+
+<style lang="scss" scoped>
+.error-text {
+  background-color: #6e303086;
+  color: white;
+  border-radius: 20px;
+  padding: 1em;
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
