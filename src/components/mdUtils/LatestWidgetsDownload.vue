0b1d3881a00f05aeb7ef57dbe2885adc0ea64504
Andrei <soprachev@mail.ru>
2024-08-27 04:01:00 +0300

    actual versions

---
+++ src/components/mdUtils/LatestWidgetsDownload.vue
@@ -0,0 +1,21 @@
+<template>
+  <a :href="asset" target="_blank" rel="noopener noreferrer">
+    <slot></slot>
+  </a>
+</template>
+
+
+<script setup lang="ts">
+import { computedAsync } from '@vueuse/core';
+import { githubRelease } from './ghRelease';
+
+const asset = computedAsync(async () => {
+  const asset = await githubRelease("https://api.github.com/repos/WOT-STAT/wotstat-widgets/releases/latest", t => t.endsWith('.wotmod'))
+  console.log(asset);
+
+  return asset.browser_download_url
+});
+
+
+
+</script>
