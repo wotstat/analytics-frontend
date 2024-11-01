c7bb631b9684ce8bf2654fe146196b583a3c5be9
Andrei <soprachev@mail.ru>
2024-11-02 02:39:32 +0300

    upd

--- src/composition/useAdditionalHeaderHeight.ts
+++ src/composition/useAdditionalHeaderHeight.ts
@@ -1,8 +1,15 @@
-import { ref } from "vue";
+import { syncRefs, useElementBounding } from "@vueuse/core";
+import { Ref, ref } from "vue";
 
 
 const additionalHeaderHeight = ref(0)
+export const headerHeight = ref(0)
 
 export function useAdditionalHeaderHeight() {
   return { additionalHeaderHeight }
+}
+
+export function useDefaultHeaderHeight(header: Ref<HTMLElement | null>) {
+  const { height } = useElementBounding(header);
+  syncRefs(height, headerHeight)
 }
