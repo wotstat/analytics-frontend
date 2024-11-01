2543c1cd3165ae0feaf0d50af4891e9d853e0fbd
Andrei <soprachev@mail.ru>
2024-11-02 02:39:32 +0300

    upd

---
+++ src/components/HeaderSpacer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div :style="{ height: headerHeight + 'px' }"></div>
+</template>
+
+
+<script setup lang="ts">
+import { headerHeight } from '@/composition/useAdditionalHeaderHeight';
+</script>
+
+
+<style lang="scss" scoped></style>
