1b59c88b3518ddedaad867b9e161d6bcdd965879
Soprachev Andrei <soprachev@mail.ru>
2025-01-04 01:00:11 +0300

    fix tooltip

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="tooltip-parent" :class="{
-    highlight: props.highlight,
+    'highlight': props.highlight,
+    'interactive': props.interactive,
     [props.pos ?? 'top']: true
   }" :style="{
-    '--offset': (props.offset ?? 3) + 'px'
+    '--offset': (props.offset ?? 3) + 'px',
+    '--delay': (props.delay ?? 0.2) + 's'
   }">
     <slot></slot>
-    <div class="tooltip">
-      <slot name="tooltip"></slot>
+    <div class="tooltip-container">
+      <div class="tooltip">
+        <slot name="tooltip"></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -18,6 +22,8 @@ const props = defineProps<{
   pos?: 'top' | 'bottom' | 'left' | 'right'
   offset?: number
   highlight?: boolean
+  interactive?: boolean
+  delay?: number
 }>()
 
 </script>
@@ -29,12 +35,13 @@ const props = defineProps<{
   margin: calc(var(--offset) * -1 - 1px);
   border-radius: 5px;
   border: 1px solid transparent;
-  transition: border .2s ease;
+  transition: border .4s ease;
 
   &:hover {
-    .tooltip {
+    .tooltip-container {
       opacity: 1;
       display: block;
+      transition-delay: var(--delay);
 
       @starting-style {
         opacity: 0;
@@ -44,33 +51,46 @@ const props = defineProps<{
 
   &.highlight {
     &:hover {
+      transition-delay: var(--delay);
       border: 1px solid #ffffff28;
     }
   }
 
-  .tooltip {
-    position: absolute;
+  &.interactive {
+    .tooltip-container {
+      pointer-events: all;
+    }
+  }
 
-    box-shadow: 0px 0px 10px 0px #242424;
-    border: 1px solid rgba(93, 93, 93, 0.737254902);
-    background-color: #353535;
-    border-radius: 10px;
-    padding: 8px;
+  .tooltip-container {
+    position: absolute;
 
     display: none;
     opacity: 0;
     transition: opacity .3s ease, display .3s ease allow-discrete;
+    padding: 3px;
 
     z-index: 3;
+
+    pointer-events: none;
+
+    .tooltip {
+      box-shadow: 0px 0px 10px 0px #242424;
+      border: 1px solid rgba(93, 93, 93, 0.737254902);
+      background-color: #353535;
+      border-radius: 10px;
+      padding: 8px;
+    }
   }
 
-  &.top .tooltip {
+  &.top .tooltip-container {
     top: 0%;
     left: 50%;
-    transform: translate(-50%, calc(-100% - 5px));
+    padding-bottom: 5px;
+    transform: translate(-50%, -100%);
   }
 
-  &.bottom .tooltip {
+  &.bottom .tooltip-container {
     bottom: -10px;
     left: 50%;
     transform: translateX(-50%);
