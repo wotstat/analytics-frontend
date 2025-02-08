d37aa031fb023c1bb2962e7cf6eb2bfffa3870ce
Andrei <soprachev@mail.ru>
2025-02-08 04:32:54 +0300

    upd

--- src/composition/usePercentProcessor.ts
+++ src/composition/usePercentProcessor.ts
@@ -25,10 +25,10 @@ export function useFixedSpaceProcessor(digits = 2) {
 
 export function useLogProcessor() {
   return (value: number) => {
-    if (value < 1e5) return value.toFixed().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-    if (value < 1e6) return (value / 1e3).toFixed(1) + 'k';
-    if (value < 1e9) return (value / 1e6).toFixed(1) + 'M';
-    if (value < 1e12) return (value / 1e9).toFixed(1) + 'B';
+    if (Math.abs(value) < 1e5) return value.toFixed().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    if (Math.abs(value) < 1e6) return (value / 1e3).toFixed(1) + 'k';
+    if (Math.abs(value) < 1e9) return (value / 1e6).toFixed(1) + 'M';
+    if (Math.abs(value) < 1e12) return (value / 1e9).toFixed(1) + 'B';
     return (value / 1e12).toFixed(1) + 'T';
   }
 }
