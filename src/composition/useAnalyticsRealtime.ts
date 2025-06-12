cd8a77b785be9f660e5de8e494e4103a8b487ad5
Andrei <soprachev@mail.ru>
2025-06-12 03:35:23 +0300

    total count fix

---
+++ src/composition/useAnalyticsRealtime.ts
@@ -0,0 +1,42 @@
+import { syncRefs, useWebSocket } from "@vueuse/core";
+import { ref, ShallowRef, shallowRef, watch } from "vue";
+
+
+type ChannelsTypes = {
+  'time': number,
+  'totalEvents': number,
+}
+
+const defaults = {
+  'time': 0,
+  'totalEvents': 0,
+}
+
+type Channels = keyof ChannelsTypes;
+
+type ChannelKey = Channels | string & {};
+
+
+type Result<T> = { data: ShallowRef<T>, hasData: ShallowRef<boolean> };
+
+export function useAnalyticsRealtime<K extends Channels, T = ChannelsTypes[K]>(channel: K): Result<T>;
+export function useAnalyticsRealtime<D>(channel: ChannelKey, defaultValue: D): Result<D>;
+export function useAnalyticsRealtime<D>(channel: ChannelKey): Result<D | null>;
+export function useAnalyticsRealtime<K extends ChannelKey, T = unknown>(channel: K, defaultValue?: T): Result<T | null> {
+
+  const def = defaultValue ?? (defaults[channel as keyof typeof defaults] as T | null)
+  const result = shallowRef<T | null>(def);
+  const hasData = ref(false);
+
+  const { data } = useWebSocket<T>(import.meta.env.VITE_ANALYTICS_REALTIME_URL + '/' + channel, { autoReconnect: true });
+
+  watch(data, (newData) => {
+    if (newData === null || newData === undefined) result.value = def;
+    else {
+      result.value = newData as T;
+      hasData.value = true
+    }
+  }, { immediate: true });
+
+  return { data: result, hasData, }
+}
