73eca20eb6b6b2eb136937273db3c0319f7f392c
Soprachev Andrei <soprachev@mail.ru>
2025-07-03 04:37:35 +0300

    animation optimization

---
+++ src/composition/utils/useNextAnimationFrameThrottle.ts
@@ -0,0 +1,22 @@
+import { ref, Ref, toValue, watch } from "vue";
+
+
+export function useNextAnimationFrameThrottle<T>(value: Ref<T>): Ref<T> {
+  const result = ref(toValue(value)) as Ref<T>
+
+  let isRegistred = false
+
+  function update() {
+    isRegistred = false
+    result.value = value.value
+  }
+
+  watch(value, () => {
+    if (isRegistred) return
+    requestAnimationFrame(() => update())
+    isRegistred = true
+  })
+
+
+  return result
+}
