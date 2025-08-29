2935786cbab03df9610ac5b13b6088d8e418504a
Andrei <soprachev@mail.ru>
2025-08-30 02:59:51 +0300

    upd

--- src/pages/shared/sidebarLayout/SidebarLayout.vue
+++ src/pages/shared/sidebarLayout/SidebarLayout.vue
@@ -37,7 +37,7 @@
 
 <script setup lang="ts">
 import { useElementBounding } from '@vueuse/core'
-import QueryPreserveRouterLink from '@/components/QueryPreserveRouterLink.vue'
+import QueryPreserveRouterLink from '@/pages/shared/sidebarLayout/QueryPreserveRouterLink.vue'
 import { computed, ref, watchEffect } from 'vue'
 import { headerHeight, useAdditionalHeaderHeight } from '@/composition/useAdditionalHeaderHeight'
 import { type SidebarLink } from './utils.ts'
