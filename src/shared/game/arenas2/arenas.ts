9d8b1edec3631fced8b7d640313f45a94dfad021
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2025-09-28 08:47:52 +0300

    arenas

--- src/shared/game/arenas2/arenas.ts
+++ src/shared/game/arenas2/arenas.ts
@@ -1,4 +1,6 @@
+import { LONG_CACHE_SETTINGS, query } from '@/db'
 import { STATIC_URL } from '@/shared/external/externalUrl'
+import { computed, ref } from 'vue'
 
 export function tagToImageName(tag: string): string {
   return tag.split('/').at(-1)?.toLowerCase() || tag.toLowerCase()
@@ -7,3 +9,106 @@ export function tagToImageName(tag: string): string {
 export function minimapUrl(tag: string, game: 'mt' | 'wot' = 'mt', format: 'webp' | 'png' = 'webp'): string {
   return `${STATIC_URL}/${game}/latest/arenas/minimap/${tag}.${format}`
 }
+
+type Arena = {
+  region: string;
+  tag: string;
+  gameplay: string;
+  season: string;
+  winnerIfExtermination: number | null;
+  winnerIfTimeout: number | null;
+  bbox: {
+    bottomLeft: { x: number; y: number };
+    upperRight: { x: number; y: number };
+  };
+  bases: {
+    team: number;
+    positions: { x: number; y: number }[];
+  }[];
+  spawn: {
+    team: number;
+    positions: { x: number; y: number }[];
+  }[];
+  poi: {
+    position: { x: number; y: number };
+    type: number;
+  }[];
+  control: Array<{ x: number; y: number }>;
+}
+
+type ArenasLatest = {
+  region: string;
+  gameVersionFull: string;
+  gameVersion: string;
+  gameVersionHash: string;
+  tag: string;
+  gameplay: string;
+  datetime: string;
+  gameVersionComp: string;
+  name: string;
+  season: string;
+
+  winnerIfTimeout: number | null;        // Nullable(UInt8)
+  winnerIfExtermination: number | null;  // Nullable(UInt8)
+
+  'bbox.bottomLeft': [number, number];   // Tuple(Float32, Float32)
+  'bbox.upperRight': [number, number];   // Tuple(Float32, Float32)
+
+  'base.team': number[];                 // Array(UInt8)
+  'base.positions': Array<Array<[number, number]>>;  // Array(Array(Tuple(Float32, Float32)))
+
+  'spawn.team': number[];                // Array(UInt8)
+  'spawn.positions': Array<Array<[number, number]>>; // Array(Array(Tuple(Float32, Float32)))
+
+  control: Array<[number, number]>;      // Array(Tuple(Float32, Float32))
+  'poi.position': Array<[number, number]>; // Array(Tuple(Float32, Float32))
+  'poi.type': number[];                  // Array(UInt8)
+};
+
+export const arenas = ref(<Arena[]>([]))
+const arenasMap = computed(() => new Map<string, Arena>(
+  arenas.value.map((arena) => [arenaKey(arena.region, arena.tag, arena.gameplay), arena])
+))
+
+function arenaKey(region: string, tag: string, gameplay: string): string {
+  return `${region}:${tag}:${gameplay}`
+}
+
+export function getArenaMeta(region: string, tag: string, gameplay: string) {
+  return arenasMap.value.get(arenaKey(region, tag, gameplay))
+}
+
+async function loadArenas() {
+  const response = await query<ArenasLatest>(`
+    select region, tag, gameplay, season, winnerIfExtermination, winnerIfTimeout,
+          "bbox.bottomLeft", "bbox.upperRight", "base.team", "base.positions", "spawn.team",
+          "spawn.positions", control, "poi.position", "poi.type"
+    from ArenasLatest
+    where region in ('RU', 'EU');
+  `, { settings: LONG_CACHE_SETTINGS })
+
+  arenas.value = response.data.map((arena) => ({
+    region: arena.region,
+    tag: arena.tag,
+    gameplay: arena.gameplay,
+    season: arena.season,
+    winnerIfExtermination: arena.winnerIfExtermination,
+    winnerIfTimeout: arena.winnerIfTimeout,
+    bbox: {
+      bottomLeft: { x: arena['bbox.bottomLeft'][0], y: arena['bbox.bottomLeft'][1] },
+      upperRight: { x: arena['bbox.upperRight'][0], y: arena['bbox.upperRight'][1] },
+    },
+    bases: arena['base.team'].map((team, i) => ({
+      team,
+      positions: arena['base.positions'][i].map(pos => ({ x: pos[0], y: pos[1] })),
+    })),
+    spawn: arena['spawn.team'].map((team, i) => ({
+      team,
+      positions: arena['spawn.positions'][i].map(pos => ({ x: pos[0], y: pos[1] })),
+    })),
+    poi: arena['poi.position'].map((pos, i) => ({ position: { x: pos[0], y: pos[1] }, type: arena['poi.type'][i] })),
+    control: arena.control.map(pos => ({ x: pos[0], y: pos[1] })),
+  }))
+}
+
+loadArenas()
