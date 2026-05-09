4ed257fd7f43b21818eba3644bd55b3009e9aa38
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2026-05-09 06:35:56 +0300

    chart

---
+++ src/pages/infographics/pages/onslaught/leaderboard/Detail.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="charts">
+    <div class="chart">
+      <h3>Очки по дням</h3>
+      <Chart ref="chartElement" />
+    </div>
+    <div class="chart">
+      <h3>Бои по дням</h3>
+      <Chart />
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import Chart from '@/shared/uiKit/chart/Chart.vue'
+import { Line, DataSource } from '@/shared/uiKit/chart/plugins/line/Line'
+import { onMounted, ref } from 'vue'
+
+const chartElement = ref<InstanceType<typeof Chart> | null>(null)
+
+const props = defineProps<{}>()
+
+const dataSource: DataSource = {
+  getPointsCount() {
+    return 50
+  },
+  getPoint(index: number) {
+    return { x: index, y: Math.random() * 100 }
+  }
+}
+
+onMounted(() => {
+  if (!chartElement.value) return
+
+  const chart = chartElement.value.chart
+
+  chart.addPlugin(new Line(dataSource))
+})
+</script>
+
+
+<style lang="scss" scoped>
+.charts {
+  display: flex;
+  gap: 20px;
+  padding: 20px 20px;
+
+  .chart {
+    flex: 1;
+    background-color: rgba(0, 0, 0, 0.602);
+    padding: 10px;
+    border-radius: 4px;
+    aspect-ratio: 2 / 1;
+
+    display: flex;
+    flex-direction: column;
+
+    h3 {
+      margin: 0 0 10px 0;
+      font-size: 16px;
+    }
+  }
+}
+</style>
---
+++ src/shared/uiKit/chart/Chart.ts
@@ -0,0 +1,98 @@
+
+export interface ChartPlugin {
+  apply?(delegate: ChartDelegate): void
+  getPaddings?(): { top: number, right: number, bottom: number, left: number }
+  render?(): void
+}
+
+export interface ChartDelegate {
+  scheduleRender(): void
+  addChild(element: SVGElement): void
+  removeChild(element: SVGElement): void
+}
+
+export class Chart {
+
+  private root: HTMLElement | null = null
+  private readonly svg: SVGSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  private readonly plugins: ChartPlugin[] = []
+
+  private readonly resizeObserver = new ResizeObserver((entries, observer) => this.onResize(entries, observer))
+
+  private rerenderScheduled = new Set<ChartPlugin>()
+  private renderCallScheduler: number | null = null
+  private readonly size = { width: 0, height: 0 }
+
+  constructor() {
+    this.svg.style.position = 'absolute'
+  }
+
+  public attach(root: HTMLElement) {
+    this.root?.removeChild(this.svg)
+    if (this.root) this.resizeObserver.unobserve(this.root)
+
+    this.root = root
+    this.root.appendChild(this.svg)
+    this.resizeObserver.observe(this.root)
+  }
+
+  public addPlugin(plugin: ChartPlugin) {
+    plugin.apply?.(this.createDelegate(plugin))
+    this.plugins.push(plugin)
+    this.scheduleRender(plugin)
+    return this
+  }
+
+  private onResize(entries: ResizeObserverEntry[], observer: ResizeObserver) {
+    if (entries.length === 0) return
+    const { contentRect } = entries[0]
+    this.size.width = contentRect.width
+    this.size.height = contentRect.height
+    this.svg.setAttribute('viewBox', `0 0 ${this.size.width} ${this.size.height}`)
+    this.render(true)
+  }
+
+  private createDelegate(plugin: ChartPlugin): ChartDelegate {
+    return {
+      scheduleRender: () => this.scheduleRender(plugin),
+      addChild: (element: SVGElement) => this.addChild(element),
+      removeChild: (element: SVGElement) => this.removeChild(element)
+    }
+  }
+
+  private addChild(element: SVGElement) {
+    this.svg.appendChild(element)
+  }
+
+  private removeChild(element: SVGElement) {
+    this.svg.removeChild(element)
+  }
+
+  private scheduleRender(plugin: ChartPlugin) {
+    this.rerenderScheduled.add(plugin)
+    this.renderOnNextFrame()
+  }
+
+  private renderOnNextFrame() {
+    if (this.renderCallScheduler != null) return
+    this.renderCallScheduler = requestAnimationFrame(() => {
+      this.renderCallScheduler = null
+      this.render()
+    })
+  }
+
+  private render(all: boolean = false) {
+    if (this.renderCallScheduler != null) {
+      cancelAnimationFrame(this.renderCallScheduler)
+      this.renderCallScheduler = null
+    }
+
+    for (const plugin of this.plugins) {
+      if (this.rerenderScheduled.has(plugin) || all) {
+        plugin.render?.()
+        this.rerenderScheduled.delete(plugin)
+      }
+    }
+    this.rerenderScheduled.clear()
+  }
+}
