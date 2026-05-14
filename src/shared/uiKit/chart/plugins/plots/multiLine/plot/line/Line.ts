079e08e959f56dd21f0700643e2fb8980d8fe8cc
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2026-05-14 04:31:10 +0300

    upd

---
+++ src/shared/uiKit/chart/plugins/plots/multiLine/plot/line/Line.ts
@@ -0,0 +1,80 @@
+import { MultiLineChart } from '../../MultiLine'
+import { Bounds } from '../../utils/Bounds'
+import { ChartSpace } from '../../utils/ChartSpace'
+import { Point } from '../../utils/Point'
+
+
+export abstract class Line {
+  protected multiLine: MultiLineChart | null = null
+  protected bounds: Bounds | null = null
+  protected isDirty = true
+
+  protected line = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+
+  private lastRenderedBoundsHash = ''
+
+  constructor(classes: string[] = []) {
+    this.line.classList.add('line', ...classes)
+  }
+
+  abstract getPointsCount(): number
+  abstract getPointAt(index: number): Point | null
+  abstract getPointsInBounds(bounds: Bounds): (Point | null)[]
+
+  protected pointsDidChange() {
+    this.isDirty = true
+    this.bounds = null
+    this.multiLine?.dataDidChange()
+  }
+
+  attach(root: SVGGElement, multiLine: MultiLineChart) {
+    this.multiLine = multiLine
+    root.appendChild(this.line)
+  }
+
+  detach() {
+    this.line.remove()
+  }
+
+  getBounds(): Bounds {
+    if (this.bounds && !this.bounds.isEmpty()) return this.bounds
+
+    this.bounds = new Bounds()
+    const pointsCount = this.getPointsCount()
+    for (let i = 0; i < pointsCount; i++) {
+      const p = this.getPointAt(i)
+      if (p) this.bounds.addPoint(p)
+    }
+    return this.bounds
+  }
+
+  render(space: ChartSpace, force: boolean = false) {
+    if (!this.isDirty && !force && this.lastRenderedBoundsHash === space.getHash()) return
+
+    const points = this.getPointsInBounds(space.bounds)
+
+    if (points.length === 0) {
+      this.line.setAttribute('d', '')
+      return
+    }
+
+    let d = ''
+    let lastPointIsNull = true
+    for (let i = 0; i < points.length; i++) {
+      const point = points[i]
+      if (point === null) {
+        lastPointIsNull = true
+        continue
+      }
+
+      const p = space.translate(point)
+      d += `${lastPointIsNull ? 'M' : 'L'} ${p.x} ${p.y} `
+      lastPointIsNull = false
+    }
+
+    this.line.setAttribute('d', d)
+
+    this.isDirty = false
+    this.lastRenderedBoundsHash = space.getHash()
+  }
+}
---
+++ src/shared/uiKit/chart/plugins/plots/multiLine/utils/Bounds.ts
@@ -0,0 +1,86 @@
+import { Point } from './Point'
+
+export class Bounds {
+  private dataCount = 0
+
+  constructor(
+    public minX: number = Infinity,
+    public maxX: number = -Infinity,
+    public minY: number = Infinity,
+    public maxY: number = -Infinity
+  ) { }
+
+  static fromPoints(points: Point[]) {
+    const bounds = new Bounds()
+    for (const p of points) bounds.addPoint(p)
+    return bounds
+  }
+
+  static fromMinMax(minX: number, maxX: number, minY: number, maxY: number) {
+    const bounds = new Bounds()
+    bounds.minX = minX
+    bounds.maxX = maxX
+    bounds.minY = minY
+    bounds.maxY = maxY
+    bounds.dataCount = 1
+    return bounds
+  }
+
+  static fromMinMaxObject({ minX, maxX, minY, maxY }: { minX: number, maxX: number, minY: number, maxY: number }) {
+    return Bounds.fromMinMax(minX, maxX, minY, maxY)
+  }
+
+  addPoint(p: Point) {
+    this.minX = Math.min(this.minX, p.x)
+    this.maxX = Math.max(this.maxX, p.x)
+    this.minY = Math.min(this.minY, p.y)
+    this.maxY = Math.max(this.maxY, p.y)
+    this.dataCount++
+  }
+
+  addXY(x: number, y: number) {
+    this.minX = Math.min(this.minX, x)
+    this.maxX = Math.max(this.maxX, x)
+    this.minY = Math.min(this.minY, y)
+    this.maxY = Math.max(this.maxY, y)
+    this.dataCount++
+  }
+
+  addX(x: number) {
+    this.minX = Math.min(this.minX, x)
+    this.maxX = Math.max(this.maxX, x)
+    this.dataCount++
+  }
+
+  addY(y: number) {
+    this.minY = Math.min(this.minY, y)
+    this.maxY = Math.max(this.maxY, y)
+    this.dataCount++
+  }
+
+  extend(bounds: Bounds) {
+    this.minX = Math.min(this.minX, bounds.minX)
+    this.maxX = Math.max(this.maxX, bounds.maxX)
+    this.minY = Math.min(this.minY, bounds.minY)
+    this.maxY = Math.max(this.maxY, bounds.maxY)
+    this.dataCount += bounds.dataCount
+  }
+
+  clone() {
+    const clone = new Bounds(this.minX, this.maxX, this.minY, this.maxY)
+    clone.dataCount = this.dataCount
+    return clone
+  }
+
+  isEmpty() {
+    return this.minX === Infinity || this.maxX === -Infinity || this.minY === Infinity || this.maxY === -Infinity || this.dataCount === 0
+  }
+
+  isEqualTo(bounds: Bounds) {
+    return this.minX === bounds.minX && this.maxX === bounds.maxX && this.minY === bounds.minY && this.maxY === bounds.maxY
+  }
+
+  getHash() {
+    return `${this.minX}_${this.maxX}_${this.minY}_${this.maxY}`
+  }
+}
