86bafd4a78d4e18da9c2ac4f1e9993f13e4f9bc4
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2026-06-21 05:23:57 +0300

    comp7Info

--- src/pages/infographics/pages/onslaught/statistics/components/Live.vue
+++ src/pages/infographics/pages/onslaught/statistics/components/Live.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="live" ref="item">LIVE</div>
-  <PopoverAutoClose :target="item" v-model="hover" :placement="['bottom-float']"
+  <Transition name="fade">
+    <div class="live" ref="item" v-if="show">LIVE</div>
+  </Transition>
+
+  <PopoverAutoClose v-if="show" :target="item" v-model="hover" :placement="['bottom-float']"
     :viewport-offset="{ top: headerOffset, bottom: 10, left: 10, right: 10 }" :arrow-size="7" :offset="{ top: 7 }"
     :class="'comp7-tooltip'">
     <div class="tooltip">
@@ -16,7 +19,9 @@ import PopoverAutoClose from '@/shared/uiKit/popover/PopoverAutoClose.vue'
 import { useElementHover } from '@vueuse/core'
 import { ref } from 'vue'
 
-const props = defineProps<{}>()
+const props = defineProps<{
+  show: boolean
+}>()
 
 const item = ref<HTMLElement | null>(null)
 const hover = useElementHover(item)
@@ -41,5 +46,16 @@ const hover = useElementHover(item)
   color: white;
   max-width: 280px;
   line-height: 1.2;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.15s, filter 0.15s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  filter: blur(3px);
 }
 </style>
